5a01b8bd4e32c3b3e5244916314046dd7f0f5276
rafioosadani <rafioosadani@gmail.com>
2025-03-12 00:36:54 +0700

    add authentication features

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_code' => 'required|string|max:36|unique:users',
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create([
+            'user_code' => $request->user_code,
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'gender' => $request->gender,
+            'password' => bcrypt($request->password),
+            'role_id' => 4,
+        ]);
+
+        if ($user) {
+            return response()->json([
+                'message' => 'User created successfully',
+                'user' => $user,
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Failed to create user',
+        ], 400);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -18,8 +29,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'role_id',
+        'user_code',
         'name',
         'email',
+        'phone',
+        'gender',
         'password',
     ];
 
