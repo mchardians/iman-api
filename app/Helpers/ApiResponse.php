98b9913c49d6a4cfc33ae933861521aeaf8f7b47
Mochammad Ardiansyah <ardianbro1310@gmail.com>
2025-05-22 20:33:01 +0700

    refactor: add http status code to ApiResponse Helper

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -15,6 +15,7 @@ class ApiResponse {
     public static function error(?string $message = null, ?string $errors = null, int $statusCode = 500) {
         return response()->json([
             "status" => "error",
+            "status_code" => $statusCode,
             "message" => $message,
             "errors" => $errors
         ], $statusCode);
