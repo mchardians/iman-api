ae3647ee173281b2f02cc64d45954dafe99a7251
Mochammad Ardiansyah <ardianbro1310@gmail.com>
2025-05-27 22:00:12 +0700

    refactor: rename `AutoCodeGeneration` trait to `AutoResourceCodeGeneration` and add transactional code generation trait
    Adds a new trait for generating transactional codes, offering more flexibility and control over code generation for transactional resources.
    
    It introduces a new trait `AutoTransactionalCodeGeneration` with related methods.
    Renames `AutoCodeGeneration` to `AutoResorceCodeGeneration` for clarity, reflecting its specific use case for resource code generation.

--- app/Traits/AutoResorceCodeGeneration.php
+++ app/Traits/AutoResorceCodeGeneration.php
@@ -4,28 +4,28 @@ namespace App\Traits;
 
 use App\Traits\HasCodeGeneration;
 
-trait AutoCodeGeneration {
+trait AutoResorceCodeGeneration {
     use HasCodeGeneration;
 
     /**
-     * Configuration for auto code generation
+     * Configuration for auto resource code generation
      * Override this method in model
      *
      * @return array
      */
-    abstract public function getCodeConfig(): array;
+    abstract public function getResourceCodeConfig(): array;
 
     /**
      * Boot method will be called automatically by Laravel
      */
-    protected static function bootAutoCodeGeneration(): void
+    protected static function bootAutoResourceCodeGeneration(): void
     {
         static::creating(function ($model) {
-            $config = $model->getCodeConfig();
+            $config = $model->getResourceCodeConfig();
 
             if (!empty($config['column']) && !empty($config['prefix'])) {
                 if (empty($model->{$config['column']})) {
-                    $model->{$config['column']} = $model->generateCode(
+                    $model->{$config['column']} = $model->generateResourceCode(
                         $config['prefix'],
                         $config['column']
                     );
---
+++ app/Traits/AutoTransactionalCodeGeneration.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Traits\HasCodeGeneration;
+
+trait AutoTransactionalCodeGeneration {
+    use HasCodeGeneration;
+
+    /**
+     * Configuration for auto resource code generation.
+     * Override this method in model
+     *
+     * @return array
+     */
+    abstract public function getTransactionalCodeConfig(): array;
+
+    protected static function bootAutoTransactionalCodeGeneration(): void {
+        static::creating(function ($model) {
+            $config = $model->getTransactionalCodeConfig();
+
+            if (!empty($config['column']) && !empty($config['transaction_prefix']) && !empty($config['resource_prefix'])) {
+                if (empty($model->{$config['column']})) {
+                    $model->{$config['column']} = $model->generateTransactionalCode(
+                        $config['transaction_prefix'],
+                        $config['resource_prefix'],
+                        $config['column']
+                    );
+                }
+            }
+        });
+    }
+}
